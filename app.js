02611553cc99394a0e70cc046ba12fb415d4c41d
Noru <akang8150@naver.com>
2023-01-22 00:20:57 +0000

    variation of v-bind dynamic variable

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const app = Vue.createApp({
-    template: "<h1>Hello {{firstName}}</h1>",
     data() {
         return {
             firstName: "John",
+            lastName: "Doe",
+            email: "john@gmail.com",
+            gender: "male",
+            picture: "https://randomuser.me/api/portraits/men/10.jpg",
         }
     }
 })
